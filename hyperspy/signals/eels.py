a225808726a244dc9eeda10afc45e7c2567eff3a
AEljarrat <aeljarrat@el.ub.edu>
2013-01-10 16:36:02 +0100

    Minor style changes

--- hyperspy/signals/eels.py
+++ hyperspy/signals/eels.py
@@ -821,7 +821,7 @@ class EELSSpectrum(Spectrum):
             #  We calculate KKT(Im(-1/epsilon))=1+Re(1/epsilon) with FFT
             #  Follows: D W Johnson 1975 J. Phys. A: Math. Gen. 8 490
             q = np.fft.fft(Im, 2*s_size, axis.index_in_array)
-            q = - q.imag / (s_size)   
+            q = - q.imag / s_size
             q[slicer] = -q[slicer]        
             q = np.fft.fft(q, axis=axis.index_in_array)
             # Final touch, we have Re(1/eps)
